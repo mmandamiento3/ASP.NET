68080c4092ffb1bd344d38b77e832c89f21ef53a
mmandamiento3 <mmandamiento3@gmail.com>
2019-04-19 01:14:56 -0500

    -Instalamos el Entity Frmaework

---
+++ EFCodeFirst2/EFCodeFirst2/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EFCodeFirst2.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext()
+            :base("ConexionPorDefecto")
+        {
+
+        }
+        public DbSet<Persona>Persona { get; set; }
+    }
+}
---
+++ EFCodeFirst2/EFCodeFirst2/Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EFCodeFirst2.Models
+{
+    public class Persona
+    {
+        
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Nacimiento { get; set; }
+        public int Edad { get; set; }
+    }
+}
