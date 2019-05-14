b963a442dec7ac42fea6b138c7dc8b2334c68563
mmandamiento3 <mmandamiento3@gmail.com>
2019-05-13 20:21:15 -0500

    Manejando Roles

--- AuthorizeyAllowAnonymous/AuthorizeyAllowAnonymous/Controllers/HomeController.cs
+++ AuthorizeyAllowAnonymous/AuthorizeyAllowAnonymous/Controllers/HomeController.cs
@@ -13,36 +13,76 @@ namespace AuthorizeyAllowAnonymous.Controllers
     {
         public ActionResult Index()
         {
-            var estaAutenticado = User.Identity.IsAuthenticated;
-            if (estaAutenticado)
-            {
-                var Nombreusuario = User.Identity.Name;
-                var id = User.Identity.GetUserId();
+            //var estaAutenticado = User.Identity.IsAuthenticated;
+            //if (estaAutenticado)
+            //{
+            //var Nombreusuario = User.Identity.Name;
+            //var id = User.Identity.GetUserId();
 
-                //Toda la informacion del Usuario
+            //Toda la informacion del Usuario
+            //using (ApplicationDbContext db = new ApplicationDbContext())
+            //{
+            ///*1era forma con EF*/var usuario = db.Users.Where(x => x.Id == id).FirstOrDefault();
+            //                   var emailConfirmado = usuario.EmailConfirmed;
+
+
+            /*El atributo usermanager nos ofrece una variedad de 
+           cosas que podemos ahcerle a los usuarios */
+            //var userManager = new UserManager<ApplicationUser>
+            //    (new UserStore<ApplicationUser>(db));
+
+            /*2da forma mas sencilla
+             con el usermanager*/ /*var usuario2=userManager.FindById(id);*/
+
+            //var user = new ApplicationUser();
+            //user.UserName = "Christian";
+            //user.Email = "dos@gmail.com";
+
+            ////Crear usuario
+            //var resultado = userManager.Create(user,"MiPasswordSecreto");
+
+            //}
+            //}
+
+
+            /*MANEJANDO ROLES*/
+            if (User.Identity.IsAuthenticated)
+            {
                 using (ApplicationDbContext db = new ApplicationDbContext())
                 {
- /*1era forma con EF*/var usuario = db.Users.Where(x => x.Id == id).FirstOrDefault();
-                    var emailConfirmado = usuario.EmailConfirmed;
+                    var idusuarioActual = User.Identity.GetUserId();
 
+                    var roleManager = new RoleManager<IdentityRole>
+                        (new RoleStore<IdentityRole>(db));
 
-                    //El atributo usermanager nos ofrece una variedad de 
-                    //cosas que podemos ahcerle a los usuarios
+                    //Crear Rol
+                    var resultado = roleManager.Create(new IdentityRole("ApruebaPrestamos"));                 
                     var userManager = new UserManager<ApplicationUser>
                         (new UserStore<ApplicationUser>(db));
 
- /*2da forma mas sencilla
-  con el usermanager*/ /*var usuario2=userManager.FindById(id);*/
+                    //Agregar usuario a rol
+                    resultado = userManager.AddToRole(idusuarioActual,"ApruebaPrestamos");
 
-                    var user = new ApplicationUser();
-                    user.UserName = "Christian";
-                    user.Email = "dos@gmail.com";
+                    //Verificamos si el Usuario esta en algun rol?
+                    var usuarioEstaEnRol = userManager.IsInRole(idusuarioActual,"ApruebaPrestamos");
+                    var usuarioEstaEnRol2 = userManager.IsInRole(idusuarioActual,"OTRO Rol");
 
-                    //Crear usuario
-                    var resultado = userManager.Create(user,"MiPasswordSecreto");
+                    //roles del Usuario
+                    var roles = userManager.GetRoles(idusuarioActual);
 
+                    //Remover a usuario de Rol
+                    resultado = userManager.RemoveFromRole(idusuarioActual,"ApruebaPrestamos");
+
+                    //Borrar ROl
+                    var RolVendedor = roleManager.FindByName("ApruebaPrestamos");
+                    roleManager.Delete(RolVendedor);
+
+
+                    
                 }
             }
+
+
             return View();
         }
         [Authorize]
